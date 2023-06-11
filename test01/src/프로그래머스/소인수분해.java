40862ec2ebe6f2a99a7c957cc41805753e6f6578
DASOM <n980805@gmail.com>
2023-06-11 23:59:59 +0900

    Add files via upload
    코딩테스트 연습

---
+++ test01/src/프로그래머스/소인수분해.java
@@ -0,0 +1,31 @@
+package 프로그래머스;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class 소인수분해 {
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	
+	        // 입력 받기
+	        System.out.print("자연수 n을 입력하세요: ");
+	        int n = scanner.nextInt();
+	
+	        // 소인수분해 수행
+	        HashSet<Integer> set = new HashSet<>();
+	        for (int i = 2; i <= n; i++) {
+	            while (n % i == 0) {
+	                set.add(i);
+	                n /= i;
+	            }
+	        }
+	        if (n != 1) set.add(n);
+	
+	        // 결과 출력
+	        System.out.println("결과: ");
+	        for (int num : set) {
+	            System.out.print(num + " ");
+	        }
+	    }
+	}
+
