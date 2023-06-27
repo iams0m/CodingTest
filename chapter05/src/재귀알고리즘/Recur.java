2d8dd85b899b370117c81e9f373917fb20f85d98
DASOM <n980805@gmail.com>
2023-06-27 12:47:12 +0900

    재귀 알고리즘 분석하기
    재귀 알고리즘 분석하기

--- chapter05/src/재귀알고리즘/Recur.java
+++ chapter05/src/재귀알고리즘/Recur.java
@@ -3,8 +3,11 @@ package 재귀알고리즘;
 import java.util.Scanner;
 
 public class Recur {
+	static int count;
+
 	// 순수 재귀 메서드
 	static void recur(int n) {
+		count++;
 		if (n > 0) {
 			recur(n - 1);
 			System.out.println(n);
@@ -14,12 +17,13 @@ public class Recur {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("정수를 입력하세요 : ");
 		int x = sc.nextInt();
-		
+
 		recur(x);
 
+		System.out.println("메서드를 호출한 회수 : " + count);
 	}
 
 }
